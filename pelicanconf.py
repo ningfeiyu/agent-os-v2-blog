eaaba183bf931cf0c80f0473545ef3665a774237
ningfeiyu <ningfeiyu@gmail.com>
2026-08-14 09:04:11 +0800

    Add pelicanconf.py

---
+++ pelicanconf.py
@@ -0,0 +1,16 @@
+
+AUTHOR = 'ling li'
+SITENAME = 'Agent OS v2.0 Blog'
+SITEURL = ''
+PATH = 'content'
+TIMEZONE = 'Asia/Shanghai'
+DEFAULT_LANG = 'zh'
+# Feed generation
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+# Blogroll
+LINKS = (('Hermes Agent', 'https://hermes-agent.nousresearch.com/'),)
+DEFAULT_PAGINATION = 10
